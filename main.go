c106148680bccd86d7b4f4795c7e8755c65b7274
Elisio Augusto <elisiouiux@gmail.com>
2026-03-22 10:55:16 +0100

    feat: fileServer

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("🚀 Servidor rodando em http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
